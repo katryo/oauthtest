e0b2c757b6ddd9eb2651957224ecf9f9bc3613c3
katryo <katoryo55@gmail.com>
2012-05-03 14:34:33 +0900

    key consealed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-		provider :twitter, 'CfgDhQFEJWAapChToS5HHQ', '5p6B9AwreYStSWmohgiOY3R6ZMXd4dnvr2t5pjFiY'
+		provider :twitter, ENV['consumer_key'], ENV['consumer_secret']
+     
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,5 +1,7 @@
 Twitter.configure do |config|
-  config.consumer_key = 'CfgDhQFEJWAapChToS5HHQ'
-  config.consumer_secret = '5p6B9AwreYStSWmohgiOY3R6ZMXd4dnvr2t5pjFiY' 
+  config.consumer_key = ENV['consumer_key']
+  config.consumer_secret = ENV['consumer_secret']
 end
+
+
 
