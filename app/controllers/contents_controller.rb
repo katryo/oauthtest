53d7f7c202dd4201a82dd481996e5dc60afcd3e9
katryo <katoryo55@gmail.com>
2012-05-04 20:36:45 +0900

    number add

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -23,23 +23,29 @@ class ContentsController < ApplicationController
       @user = Twitter.user.screen_name
       @random_number = Time.now.sec % 4
     if params[:button_1]
-      if @random_number = 0 
-         Twitter.update("にせほー！にせほー！そこにいるの？おしいー！おしいー！明日があるの！ 3分間＼ハイ！／リプライを＼ハイ！／か・ぞ・え・て・たー＼人☆工☆知☆能☆／＼( ゜ヮ゜)＞＼(゜ヮ゜)／ ＼(゜ヮ゜)／ ＜(゜ヮ＾ )／ #にせほボタン http://nisehobutton.heroku.com/")
+
+       if @random_number = 2 
+         Twitter.update("にせほー！　にせほー！　そこにいるの？おしいー！　おしいー！　明日があるの！　3分間＼ハイ！／リプライを＼ハイ！／か・ぞ・え・て・たー＼人☆工☆知☆能☆／＼( ゜ヮ゜)＞＼(゜ヮ゜)／ ＼(゜ヮ゜)／ ＜(゜ヮ＾ )／ #にせほボタン http://nisehobutton.heroku.com/")
       elsif
-        Twitter.update("@nisehorn @nisehorrrn @nisehorrrrn にせほー #にせほボタン http://nisehobutton.heroku.com/") 
+        Twitter.update("@nisehorn @nisehorrn @nisehorrrn @nisehorrrrn にせほー #にせほボタン http://nisehobutton.heroku.com/") 
       end
-    
+    button_pushed_1 = true
+
     elsif params[:button_2]
-      if @random_number = 0 
+      if @random_number = 3
       Twitter.update("ロックスターはオワコン。時代はレッドブル #にせほボタン http://nisehobutton.heroku.com/")
       elsif
       Twitter.update("ロックスター・エナジードリンクなう #にせほボタン http://nisehobutton.heroku.com/")
       end
+
+    button_pushed_2 = true
     elsif params[:button_3] 
       Twitter.update("ゆ #にせほボタン http://nisehobutton.heroku.com/") 
+    button_pushed_3 = true
     elsif params[:button_4]
       Twitter.update("#zekitterは神 #にせほボタン http://nisehobutton.heroku.com/") 
  
+    button_pushed_4 = true 
 
     else
       #Twitter.update("アイエエエ！？") 
